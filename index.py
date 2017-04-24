d4301123fceb30840bb57f6cebf167a3ba134a19
jiravani <justin.iravani@1strategy.com>
2017-04-24 11:51:28 -0600

    added files, made variable names more verbose

--- index.py
+++ index.py
@@ -4,16 +4,17 @@ import boto3
 inuser = boto3.session.Session(profile_name='inContact-User')
 client = inuser.client('ec2')
 response = client.describe_regions()
-r1 = response.get('Regions', {})
-for x in r1:
-    cl = inuser.client('ec2', region_name=x["RegionName"])
-    response = cl.describe_vpcs()
-    v = response.get('Vpcs', {})
-    for z in v:
-        p = map(z.pop, ['VpcId','CidrBlock','IsDefault'])
-        if p[2]:
-            print "I will be deleting", x["RegionName"], p
+regions = response.get('Regions', {})
+
+for region in regions:
+    vpc_client = inuser.client('ec2', region_name=region["RegionName"])
+    response = vpc_client.describe_vpcs()
+    vpcs = response.get('Vpcs', {})
+    for vpc in vpcs:
+        vpc_id = map(vpc.pop, ['VpcId', 'CidrBlock', 'IsDefault'])
+        if vpc_id[2]:
+            print "I will be deleting", vpc["RegionName"], vpc_id
             #response_delete_vpc = cl.delete_vpc(DryRun=True,VpcId=p[0])
             #print response_delete_vpc
         else:
-            print "I will NOT be deleting", x["RegionName"], p
+            print "I will NOT be deleting", vpc["RegionName"], vpc_id
